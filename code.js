19be6736942036f5432cefdaa408c50b3bdebb85
ilvdrskn <vladalievaliev@gmail.com>
2023-01-28 18:33:19 +0400

    sdsadas

--- code.js
+++ code.js
@@ -1,8 +1,10 @@
 window.onload = function() {
     document.getElementsByClassName("svgMenu")[0].onclick = function() {
         document.getElementsByClassName("modal-menu")[0].classList.remove("hide");
+        document.getElementById("container").classList.add("dark");
         document.getElementsByClassName("svgClose")[0].onclick = function() {
             document.getElementsByClassName("modal-menu")[0].classList.add("hide");
+            document.getElementById("container").classList.remove("dark");
         }
     }
 }
